5e95fbb7384bcb97f6f21b39c508feddca49b7d9
tankcccc <540153461@qq.com>
2019-09-01 22:34:07 +0800

    Header组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+     Header
   }
 }
 </script>
@@ -21,8 +19,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
